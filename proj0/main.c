df64dee10958598bcde04b5e354f8b58ff8b1830
stephensp <stephensportia@gmail.com>
2015-01-14 12:20:06 -0800

    changed main.c

--- proj0/main.c
+++ proj0/main.c
@@ -3,12 +3,5 @@
 
 int main(int argc, char *argv[]) {
 
-	if(argc < 2) {
-		printf("Error: incorrect number of arguments\n");
-		return 0;
-	}
-	if(argc == 2) {
-		printf("%s\n", argv[0]);
-	}
 	return 0;
 }
