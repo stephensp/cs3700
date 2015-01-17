8449b2d532def0d8b34042ebb3cce5d5f7b2c7e6
stephensp <stephensportia@gmail.com>
2015-01-17 12:59:15 -0800

    Added connect/close code, connect is currently returning a failing status

--- proj0/client.c
+++ proj0/client.c
@@ -1,6 +1,10 @@
 // client.c
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+#include <netdb.h>
+#include <string.h>
+#include <unistd.h>
 #include "client.h"
 
 void setPort(client *c, int port) {
@@ -16,7 +20,12 @@ void setHostname(client *c, char *hostname) {
 	c->hostname = hostname;
 }
 void clientRun(client *c) {
+	struct sockaddr_in soc;
+	struct hostent *host;
 	int sockfd;
+	int status;
+
+	// Let's first create a socket
 	sockfd = socket(PF_INET, SOCK_STREAM, 0);
 
 	if(sockfd == -1) {
@@ -24,4 +33,24 @@ void clientRun(client *c) {
 		return;
 	}
 	
+	// Now we need to get the host IP addr
+	host = gethostbyname(c->hostname);
+	if(host == NULL) {
+		printf("Error: could not connect to host\n");
+		return;
+	}
+	// Set socket info
+	soc.sin_family = htons(AF_INET);
+	soc.sin_port = htons(c->port);
+	memcpy(&(soc.sin_addr.s_addr), host->h_addr, host->h_length);
+
+	// Next connect client socket to server socket
+	status = connect(sockfd, (struct sockaddr *) &soc, sizeof(struct sockaddr_in));
+	
+	if(status == -1) {
+		printf("Error: failed to connect to socket\n");
+		return;
+	}
+	// Let's make sure to close the connection
+	close(sockfd);	
 }
